c34518e7e711137ed95cd720656b427687a20d39
Olga Sh <shei-olya@mail.ru>
2023-02-22 15:35:21 +0300

    Homework_10

---
+++ HM_10/dataHM.js
@@ -0,0 +1,261 @@
+const dataInfo = `[{
+    "results": [
+      {
+        "gender": "male",
+        "name": {
+          "title": "Mr",
+          "first": "Juan",
+          "last": "Bell"
+        },
+        "location": {
+          "street": {
+            "number": 5002,
+            "name": "Timber Wolf Trail"
+          },
+          "city": "Gladstone",
+          "state": "Western Australia",
+          "country": "Australia",
+          "postcode": 8642,
+          "coordinates": {
+            "latitude": "-82.7053",
+            "longitude": "-140.2468"
+          },
+          "timezone": {
+            "offset": "-12:00",
+            "description": "Eniwetok, Kwajalein"
+          }
+        },
+        "email": "juan.bell@example.com",
+        "login": {
+          "uuid": "8008d6fc-ae26-4f32-8979-252ee2fbdbc7",
+          "username": "heavymeercat231",
+          "password": "miguel",
+          "salt": "P3jz8ojW",
+          "md5": "af8938a36d101a798dea8f38caed76bf",
+          "sha1": "11809de5ca22026079660a93d8418564ee7795c2",
+          "sha256": "f1b5265367a074bf233598e9868cf217b77e86d261b72ba5f9113741b9fe6d81"
+        },
+        "dob": {
+          "date": "1965-03-14T11:06:09.801Z",
+          "age": 57
+        },
+        "registered": {
+          "date": "2005-05-12T10:05:16.619Z",
+          "age": 17
+        },
+        "phone": "09-1978-7747",
+        "cell": "0444-261-733",
+        "id": {
+          "name": "TFN",
+          "value": "087160542"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/92.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/92.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/92.jpg"
+        },
+        "nat": "AU"
+      }
+    ],
+    "info": {
+      "seed": "df1370da16fb38e9",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  },{
+    "results": [
+      {
+        "gender": "female",
+        "name": {
+          "title": "Miss",
+          "first": "Derci",
+          "last": "da Mota"
+        },
+        "location": {
+          "street": {
+            "number": 1864,
+            "name": "Beco dos Namorados"
+          },
+          "city": "Guarulhos",
+          "state": "Alagoas",
+          "country": "Brazil",
+          "postcode": 76914,
+          "coordinates": {
+            "latitude": "-81.0747",
+            "longitude": "10.1876"
+          },
+          "timezone": {
+            "offset": "+7:00",
+            "description": "Bangkok, Hanoi, Jakarta"
+          }
+        },
+        "email": "derci.damota@example.com",
+        "login": {
+          "uuid": "343c2151-be88-4977-b77c-bdad03590289",
+          "username": "silverpanda841",
+          "password": "jjjjj",
+          "salt": "6FILBD8D",
+          "md5": "4d33bc88ba2c55b1822e7175fea4c20c",
+          "sha1": "15fe2efb206774da48b27debe7b087a35af6e595",
+          "sha256": "2a5276b90173bb200bdfa8ac6be8dc1c58c8472699874ff863a4b242600d5851"
+        },
+        "dob": {
+          "date": "1980-03-28T18:41:45.863Z",
+          "age": 42
+        },
+        "registered": {
+          "date": "2019-02-19T07:25:00.258Z",
+          "age": 4
+        },
+        "phone": "(23) 5015-6598",
+        "cell": "(09) 0925-2004",
+        "id": {
+          "name": "CPF",
+          "value": "374.813.101-52"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/women/1.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/women/1.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/women/1.jpg"
+        },
+        "nat": "BR"
+      }
+    ],
+    "info": {
+      "seed": "ef0f214aafbf1e13",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  },{
+    "results": [
+      {
+        "gender": "male",
+        "name": {
+          "title": "Mr",
+          "first": "Dhruv",
+          "last": "Shet"
+        },
+        "location": {
+          "street": {
+            "number": 4925,
+            "name": "Kazimar St"
+          },
+          "city": "Ajmer",
+          "state": "Manipur",
+          "country": "India",
+          "postcode": 67044,
+          "coordinates": {
+            "latitude": "7.5811",
+            "longitude": "-110.1375"
+          },
+          "timezone": {
+            "offset": "-3:30",
+            "description": "Newfoundland"
+          }
+        },
+        "email": "dhruv.shet@example.com",
+        "login": {
+          "uuid": "5d47609a-f8f5-4692-a8cb-534a30a8c83a",
+          "username": "smallpanda429",
+          "password": "kerstin",
+          "salt": "8H0uzJky",
+          "md5": "02727e6e87435fc99186377b937c5b73",
+          "sha1": "00c601b6b5a9881564ff07067f776aee08ed5ca4",
+          "sha256": "1a4e5db24baa8a5d42b430b17ebbfe2b0f0b52abea00e4f10acbf3b643a0c44b"
+        },
+        "dob": {
+          "date": "1955-10-11T13:19:00.288Z",
+          "age": 67
+        },
+        "registered": {
+          "date": "2007-06-30T22:50:59.080Z",
+          "age": 15
+        },
+        "phone": "7065752606",
+        "cell": "8113249650",
+        "id": {
+          "name": "UIDAI",
+          "value": "392217203782"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/68.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/68.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/68.jpg"
+        },
+        "nat": "IN"
+      }
+    ],
+    "info": {
+      "seed": "d826a4e80d280c6e",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  },{
+    "results": [
+      {
+        "gender": "male",
+        "name": {
+          "title": "Monsieur",
+          "first": "Marlon",
+          "last": "Fontai"
+        },
+        "location": {
+          "street": {
+            "number": 7864,
+            "name": "Rue Bossuet"
+          },
+          "city": "Fahrni",
+          "state": "Appenzell Innerrhoden",
+          "country": "Switzerland",
+          "postcode": 4166,
+          "coordinates": {
+            "latitude": "-60.5466",
+            "longitude": "-17.9418"
+          },
+          "timezone": {
+            "offset": "-6:00",
+            "description": "Central Time (US & Canada), Mexico City"
+          }
+        },
+        "email": "marlon.fontai@example.com",
+        "login": {
+          "uuid": "a43c641b-28db-40ec-ad17-9a6a5c7783d3",
+          "username": "bigkoala638",
+          "password": "stars",
+          "salt": "jG2Ra3VM",
+          "md5": "690905fc8686c92c3c00f6b57dc23750",
+          "sha1": "47a63c88487ea9cfbf1efde6f9f90cb8ed44f218",
+          "sha256": "415cd4d76a31ad252b9b0f73a73e5a0732a9d71524895a7c7c82d52f06e88ed9"
+        },
+        "dob": {
+          "date": "1982-06-12T02:13:39.115Z",
+          "age": 40
+        },
+        "registered": {
+          "date": "2009-10-24T15:24:35.186Z",
+          "age": 13
+        },
+        "phone": "078 445 28 23",
+        "cell": "075 158 14 73",
+        "id": {
+          "name": "AVS",
+          "value": "756.2665.9736.72"
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/67.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/67.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/67.jpg"
+        },
+        "nat": "CH"
+      }
+    ],
+    "info": {
+      "seed": "ffad2f5caa0bb501",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }]`
